5f830e81c28022c82e7130f57949d79622fac0b7
Giao <gnguyen@ipllc.co>
2025-07-30 19:30:33 -0600

    updated maverics config with service extension for task 4

--- Task 4/email.go
+++ Task 4/email.go
@@ -10,24 +10,21 @@ import (
 	"github.com/strata-io/service-extension/orchestrator"
 )
 
+
 type User []struct { //structure for user info 
 	ID       	int    `json:"id"`
 	Email    	string `json:"email"`
 }
 
-func CreateEmailHeader (
-    api orchestrator.Orchestrator,
-    _ http.ResponseWriter,
-    _ *http.Request,
-) (http.Header, error) {
-    logger := api.Logger()
-    logger.Info("se", "building email custom header")
-    session, err := api.Session()
-    if err != nil {
-        return nil, fmt.Errorf("unable to retrieve Orchestrator session: %w", err)
-    }
-
-    logger.Debug("se", "retrieving email from mock endpoint..")
+func CreateEmailHeader(api orchestrator.Orchestrator, _ http.ResponseWriter, _ *http.Request) (http.Header, error) {
+	logger := api.Logger()
+	logger.Debug("se", "building email custom header")
+	
+	session, err := api.Session()
+	if err != nil {
+		logger.Error("se", "unable to retrieve session", "error", err.Error())
+		return nil, err
+	}
 
     url := "https://jsonplaceholder.typicode.com/users?id=2" //define url to retrieve user information by getting userId input
 	response, err := http.Get(url) //checking url is up and retrieve user information, if it fails throw an error
@@ -42,14 +39,11 @@ func CreateEmailHeader (
 	}
     
 	var userInfo User //declare userInfo variable and assign User struct to it
+	json.Unmarshal(body, &userInfo)
 
-	if jsonErr := json.Unmarshal(body, &userInfo); jsonErr != nil { //check for parsing error
-		fmt.Printf("Couldn't parse JSON", jsonErr)
-		return
-	}
 	fmt.Println(userInfo[0].Email) //display GET response for email, in an array 0 since it returns only one entry
 
-    email := userInfo[0].email
+    email := userInfo[0].Email
     header := make(http.Header)
     header["CUSTOM-EMAIL"] = []string{email}
     return header, nil
